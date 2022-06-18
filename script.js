31e9070d5fc2c8f1ad3fbf5dafa3929a9399598d
Natasha Matthews <natashamatthews90@gmail.com>
2022-06-18 16:32:03 -0500

    displaying api quote to ui

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQutoes = [];
 
 // Show New Quote
 function newQuote() {
   //Pick a random quote from apiQuotes array
   const quote = apiQutoes[Math.floor(Math.random() * apiQutoes.length)];
-  console.log(quote);
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
