267c361826464d447403cef4cdcce46e83045b03
Natasha Matthews <natashamatthews90@gmail.com>
2022-06-18 16:22:31 -0500

    added the quotes api

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let apiQutoes = [];
 
+// Show New Quote
+function newQuote() {
+  //Pick a random quote from apiQuotes array
+  const quote = apiQutoes[Math.floor(Math.random() * apiQutoes.length)];
+  console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQutoes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// On Load
+getQuotes();
